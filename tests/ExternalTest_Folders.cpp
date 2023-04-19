95235698a88a8d281086df4ebbf5d7da6bbad933
SergeyMC9730 <bdsservers@yandex.ru>
2023-04-19 22:26:50 +0800

    Add folder test

--- tests/ExternalTest_Folders.cpp
+++ tests/ExternalTest_Folders.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+#include <filesystem>
 
 int main(int argc, char *argv[]) {
-    std::cout << argv[0];
+    if (argc == 1) {
+        std::cout << "Error: no folder selected" << std::endl;
+        return -1;
+    }
+
+    std::string folder = argv[1];
+
+    int files = 0;
+
+    std::cout << "Folder: " + folder << std::endl;
+
+    try {
+        for (const auto & entry : std::filesystem::directory_iterator(folder)) files++;
+    } catch (std::filesystem::filesystem_error) {
+        std::cout << "Error: no such directory" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Files: " << files << std::endl;
+
     return 0;
 }
