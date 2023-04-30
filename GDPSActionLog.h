a320b368619221345ead013652b08b0eb039daab
SergeyMC9730 <bdsservers@yandex.ru>
2023-04-30 17:27:36 +0800

    GDPS action log

---
+++ GDPSActionLog.h
@@ -0,0 +1,17 @@
+#pragma once
+
+enum GDPSActionType {
+    GATRate = 0,
+    GATFeature, GATEpic, GATGodlike, GATLegendary,
+    GATCoins
+};
+
+class GDPSActionLog {
+public:
+    std::string m_sModerator;
+    GDPSActionType m_eType;
+    std::string m_sValue1;
+    std::string m_sValue2;
+    int m_nLevelID;
+    std::string m_sTimestamp;
+};
