a76f772cccf2b83cad74f2181aed44af037b046c
SergeyMC9730 <bdsservers@yandex.ru>
2023-07-06 15:52:39 +0800

    sqlite test

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include "Level.h"
 
 #include "TestingBoomlings22.h"
+// #include "connection.h"
 #include "requests/v1.helloworld.h"
 #include "requests/v1.level.download.h"
 #include "requests/v1.img.request.h"
@@ -24,9 +25,17 @@
 
 #include "GDServer_BoomlingsLike21.h"
 
+// #include "sqlite3/connection.h"
+// #include "sqlite3/connection_config.h"
+
+#include <sqlite3.h>
+
 #include "tests.h"
 
 #include "Translation.h"
+
+// #include <sqlpp11/sqlpp11.h>
+// #include <sqlpp11/sqlite3/sqlite3.h>
 
 using namespace LevelAPI;
 using namespace LevelAPI::Frontend::Translation;
@@ -125,54 +134,71 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    DatabaseController::setup();
-    HttpController::setup();
-    HttpController::parse();
+    std::cout << "doing sqlite shit pls wait" << std::endl;
 
-    LevelAPI::Tests::testCurl();
-    // LevelAPI::Tests::testGDParsers();
+    sqlite3 *sq;
+    sqlite3_open("database/test.db", &sq);
 
-    webserver ws = create_webserver()
-        .port(HttpController::getPort())
-        .max_threads(HttpController::getThreads())
-        .memory_limit(256 * 1024)
-        .debug()
-        // .file_upload_target(FILE_UPLOAD_MEMORY_AND_DISK)
-    ;
+    if (!sq) {
+        std::cout << "error happened!" << std::endl;
+    } else {
+        sqlite3_close(sq);
+    }
 
-    // std::thread t([&]{
-    //     int i = 0;
-    //     int size = DatabaseController::database->m_vNodes.size();
-    //     while(i < size) {
-    //         std::string leveldir = "database/nodes/" + DatabaseController::database->m_vNodes[i]->m_sInternalName + "/levels";
-    //         DIR *d = opendir(leveldir.c_str());
-    //         struct dirent *dir;
-    //         auto nd = DatabaseController::database->getNode("boomlings");
+    // sqlpp::connection con;
+    
+    // auto config = sqlpp::sqlite3::connection_config("database/test.db");
+
+    // sqlpp::sqlite3::connection con(config);
+
+    // DatabaseController::setup();
+    // HttpController::setup();
+    // HttpController::parse();
+
+    // LevelAPI::Tests::testCurl();
+    // // LevelAPI::Tests::testGDParsers();
+
+    // webserver ws = create_webserver()
+    //     .port(HttpController::getPort())
+    //     .max_threads(HttpController::getThreads())
+    //     .memory_limit(256 * 1024)
+    //     .debug()
+    //     // .file_upload_target(FILE_UPLOAD_MEMORY_AND_DISK)
+    // ;
+
+    // // std::thread t([&]{
+    // //     int i = 0;
+    // //     int size = DatabaseController::database->m_vNodes.size();
+    // //     while(i < size) {
+    // //         std::string leveldir = "database/nodes/" + DatabaseController::database->m_vNodes[i]->m_sInternalName + "/levels";
+    // //         DIR *d = opendir(leveldir.c_str());
+    // //         struct dirent *dir;
+    // //         auto nd = DatabaseController::database->getNode("boomlings");
             
-    //         while ((dir = readdir(d)) != NULL) {
-    //             int id = std::atoi(dir->d_name + 6);
-    //             //std::cout << "Updating level " << id << std::endl;
-    //             auto level = nd->getLevel(id);
-    //             if (level != nullptr) {
-    //                 nd->initLevel(level);
-    //                 delete level;
-    //             }
-    //         }
-    //         closedir(d);
-    //         i++;
-    //     }
-    // });
+    // //         while ((dir = readdir(d)) != NULL) {
+    // //             int id = std::atoi(dir->d_name + 6);
+    // //             //std::cout << "Updating level " << id << std::endl;
+    // //             auto level = nd->getLevel(id);
+    // //             if (level != nullptr) {
+    // //                 nd->initLevel(level);
+    // //                 delete level;
+    // //             }
+    // //         }
+    // //         closedir(d);
+    // //         i++;
+    // //     }
+    // // });
 
-    // t.detach();
+    // // t.detach();
 
-    ws.register_resource("/api/v1/hello", reinterpret_cast<http_resource *>(new LevelAPI::v1::HelloWorldRequest()));
-    ws.register_resource("/api/v1/level/download", reinterpret_cast<http_resource *>(new LevelAPI::v1::LevelDownloadRequest()));
-    ws.register_resource("/api/v1/img/request/{file}", reinterpret_cast<http_resource *>(new LevelAPI::v1::IMGRequest()));
-    ws.register_resource("/api/v1/res/request/{file}", reinterpret_cast<http_resource *>(new LevelAPI::v1::ResourceRequest()));
+    // ws.register_resource("/api/v1/hello", reinterpret_cast<http_resource *>(new LevelAPI::v1::HelloWorldRequest()));
+    // ws.register_resource("/api/v1/level/download", reinterpret_cast<http_resource *>(new LevelAPI::v1::LevelDownloadRequest()));
+    // ws.register_resource("/api/v1/img/request/{file}", reinterpret_cast<http_resource *>(new LevelAPI::v1::IMGRequest()));
+    // ws.register_resource("/api/v1/res/request/{file}", reinterpret_cast<http_resource *>(new LevelAPI::v1::ResourceRequest()));
 
-    std::cout << getByKey("lapi.main.portstart", HttpController::getPort()) << std::endl;
+    // std::cout << getByKey("lapi.main.portstart", HttpController::getPort()) << std::endl;
 
-    ws.start(true);
+    // ws.start(true);
 
     return 0;
 }
