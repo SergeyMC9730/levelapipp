7bb5e2067be64c603e31e3a13f99b97a7ab8c141
SergeyMC9730 <bdsservers@yandex.ru>
2024-04-09 15:23:10 +0800

    add comments for DPP frontend

--- DCommand.h
+++ DCommand.h
@@ -30,44 +30,65 @@
 
 namespace LevelAPI {
     namespace Frontend {
+	// how command has been executed (using /command, [button] or _select menu_
         enum DCommandEventType {
+	    // command has been executed by using /command
             ET_Command = 0,
+            // command has been executed by using [button]
             ET_Button = 1,
+            // command has been executed by using select menu popup
             ET_SelectMenu = 2
         };
 
+	// frontend for DPP command
         class DCommand {
         public:
             using DCommandCallback = std::function<void(DCommand *, DCommandEvent *event)>;
 
+	    // linked cluster
             dpp::cluster *_cluster;
         protected:
-
+	    // instance of the command
             dpp::slashcommand _instance;
 
+	    // all logic happens inside this callback.
+	    // just a unified callback for three events described in DCommandEventType.
             DCommandCallback _callback;
 
+	    // discord bot id
             dpp::snowflake _botID;
 
+            // returns empty callback by default.
+	    // class that inherits DCommand can replace empty function with own data
             virtual DCommandCallback getDefaultCallback();
 
+            // available events
             std::vector<std::pair<std::string, DCommandEventType>> _events;
 
+            // all event logic happens here
             virtual void eventCallback(std::string name, DCommandEvent *event);
 
+            // registers specific event for this command.
+	    // it's useful when adding buttons and popup menues for this DCommand
             void registerEvent(std::string name, DCommandEventType type);
         public:
             DCommand(dpp::snowflake botID, DCommandCallback callback);
             ~DCommand();
 
+	    // get _instance
             const dpp::slashcommand getCommand();
-            
+
+	    // run _callback with slashcommand
             virtual void run(const dpp::slashcommand_t &event);
+            // run _callback with button_click
             virtual void run(const dpp::button_click_t &event);
+	    // run _callback with select_click
             virtual void run(const dpp::select_click_t &event);
 
+	    // get name of this DCommand
             virtual std::string getCommandName();
 
+	    // get all registered by DCommand events
             const std::vector<std::pair<std::string, DCommandEventType>> getRegistedEvents();
         };
     }
