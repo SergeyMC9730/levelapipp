ba0b040adfd594ec0e860a577d93f833b80d8e61
SergeyMC9730 <bdsservers@yandex.ru>
2024-05-30 22:26:30 +0800

    i finally found a memory leak

--- gmd2pp/impl.cpp
+++ gmd2pp/impl.cpp
@@ -114,6 +114,7 @@ void GMD2::generate() {
                 char *buf = (char *)malloc(128);
                 sprintf(buf, "%d", (this->m_pLevel->m_nSongID == 0) ? this->m_pLevel->m_nMusicID : this->m_pLevel->m_nSongID);
                 song += buf;
+                free(buf);
             song += "</i>";
         song += "<k>k13</k><t/><k>k21</k><i>2</i><k>k50</k><i>35</i>";
 
